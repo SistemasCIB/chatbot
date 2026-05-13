ed4e28f0dc9b92fd56b3eeaf306e06c57a80acfa
Isabela Correa <aprendizti@cib.org.co>
2026-05-13 12:00:19 -0500

    Outlook Calendar

--- prueba.py
+++
@@ -1,5 +0,0 @@
-from services.outlook import obtener_token
-
-token = obtener_token()
-
-print(token)
--- services/outlook.py
+++ services/outlook.py
@@ -1,17 +1,21 @@
+import datetime
 import os
 import requests
 
 from msal import ConfidentialClientApplication
+from dotenv import load_dotenv
 
-CLIENT_ID = os.getenv("MICROSOFT_CLIENT_ID")
-CLIENT_SECRET = os.getenv("MICROSOFT_CLIENT_SECRET")
-TENANT_ID = os.getenv("MICROSOFT_TENANT_ID")
+load_dotenv(".env")
+
+CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
+TENANT_ID = os.getenv("AZURE_TENANT_ID")
 
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 
 SCOPES = ["https://graph.microsoft.com/.default"]
 
-GRAPH_URL = "https://graph.microsoft.com/v1.0/users/TU_CORREO@empresa.com/events"
+GRAPH_URL = "https://graph.microsoft.com/v1.0/users/Sistemas@cib.org.co/events"
 
 def obtener_token():
 
@@ -26,3 +30,55 @@ def obtener_token():
     )
 
     return result['access_token']
+
+def crear_evento_outlook(cita):
+
+    token = obtener_token()
+
+    inicio = datetime.strptime(
+        f"{cita.fecha_cita.strftime('%Y-%m-%d')} {cita.hora_cita}",
+        '%Y-%m-%d %H:%M'
+    )
+
+    fin = inicio + datetime.timedelta(hours=1)
+
+    evento = {
+
+        "subject": f"Cita - {cita.paciente.nombre}",
+
+        "start": {
+            "dateTime": inicio.isoformat(),
+            "timeZone": "America/Bogota"
+        },
+
+        "end": {
+            "dateTime": fin.isoformat(),
+            "timeZone": "America/Bogota"
+        },
+
+        "body": {
+            "contentType": "HTML",
+            "content": f"""
+                Paciente: {cita.paciente.nombre}<br>
+                Documento: {cita.paciente.documento}<br>
+                Teléfono: {cita.paciente.telefono}<br>
+                Tipo examen: {cita.tipo_examen}
+            """
+        }
+    }
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(
+        GRAPH_URL,
+        headers=headers,
+        json=evento
+    )
+
+    return response.json()
+
+print("CLIENT:", CLIENT_ID)
+print("TENANT:", TENANT_ID)
